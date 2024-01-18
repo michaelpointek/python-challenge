0316863decb32ee647192cf30cf524c5267077fa
michaelpointek <michaelpointek@gmail.com>
2024-01-18 13:30:47 -0500

    Create main.py
    This is the source code for the PyPoll script.

---
+++ PyPoll/main.py
@@ -0,0 +1,10 @@
+# Import and name reference file
+import csv
+csv_file_path = C:\Users\micha\OneDrive\Documents\GitHub\python-challenge\PyPoll\Resources\election_data.csv
+
+# Initialize variables to store data 
+total_months = 0
+
+# Open and read CSV file
+with open(csv_file_path, newline='') as csv_file_path   
+    csvreader = csv.reader(csvfile, delimiter=',')
